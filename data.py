04f2dfa26693071a52a765059e58298328805101
tondach01 <ondra.metelka@seznam.cz>
2023-10-26 23:48:20 +0200

    dataframe for words

--- data.py
+++ data.py
@@ -5,13 +5,40 @@ def episodes():
     return pd.read_csv("episodes.csv")
 
 
-def replicas():
+def replicas_base():
     df = pd.read_csv("replicas.csv")
 
-    c = df.groupby("Character").count()
-    # TODO
+    df["Character"] = df["Character"].apply(lambda x: "Amy" if x in ["1. Amy", "2. Amy", "4. Amy"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Bernadette" if x in ["Bermadette"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Howard" if x in ["Howatd"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Leonard" if x in ["Leoanard"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Raj" if x in ["Rai", "Rajj"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Sheldon" if x in ["1. Sheldon", "3. Sheldon", "4. Sheldon",
+                                                                         "5. Sheldon", "Sehldon", "Sgeldon",
+                                                                         "Shedon", "Sheldon on laptop screen",
+                                                                         "Shelldon", "Shldon"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Other" if x not in ["Amy", "Bernadette", "Howard", "Leonard",
+                                                                           "Penny", "Raj", "Sheldon"] else x)
+
     return df
 
 
+def replicas_words(base: pd.DataFrame):
+    df = base.copy(deep=True)
+    df["Word"] = df["Replica"].apply(lambda x: str(x).split())
+    df.drop(columns=["Replica"], inplace=True)
+    df = df.explode("Word", ignore_index=True)
+    df["Word"] = df["Word"].apply(lambda x: str(x).strip(".,!?-:\"…"))
+    # TODO mask the most common words
+    return df
+
+
+def replicas_interactions(base: pd.DataFrame):
+    # pass a copy here
+    # TODO
+    pass
+
+
 if __name__ == "__main__":
-    replicas()
+    b = replicas_base()
+    replicas_words(b)

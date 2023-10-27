cb707fc21e20388b8c2638d6f22ccb0ce8ad4782
tondach01 <ondra.metelka@seznam.cz>
2023-10-27 11:55:20 +0200

    merge words with lowercase variant

--- data.py
+++ data.py
@@ -12,13 +12,15 @@ def replicas_base():
     df["Character"] = df["Character"].apply(lambda x: "Bernadette" if x in ["Bermadette"] else x)
     df["Character"] = df["Character"].apply(lambda x: "Howard" if x in ["Howatd"] else x)
     df["Character"] = df["Character"].apply(lambda x: "Leonard" if x in ["Leoanard"] else x)
-    df["Character"] = df["Character"].apply(lambda x: "Raj" if x in ["Rai", "Rajj"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Raj" if x in ["Rai", "Rajj", "Ra"] else x)
     df["Character"] = df["Character"].apply(lambda x: "Sheldon" if x in ["1. Sheldon", "3. Sheldon", "4. Sheldon",
                                                                          "5. Sheldon", "Sehldon", "Sgeldon",
                                                                          "Shedon", "Sheldon on laptop screen",
                                                                          "Shelldon", "Shldon"] else x)
+    df["Character"] = df["Character"].apply(lambda x: "Scene" if x in ["Sceme", "Secne"] else x)
+
     df["Character"] = df["Character"].apply(lambda x: "Other" if x not in ["Amy", "Bernadette", "Howard", "Leonard",
-                                                                           "Penny", "Raj", "Sheldon"] else x)
+                                                                           "Penny", "Raj", "Sheldon", "Scene"] else x)
 
     return df
 
@@ -29,6 +31,8 @@ def replicas_words(base: pd.DataFrame):
     df.drop(columns=["Replica"], inplace=True)
     df = df.explode("Word", ignore_index=True)
     df["Word"] = df["Word"].apply(lambda x: str(x).strip(".,!?-:\"…"))
+    val = set(df["Word"].values)
+    df["Word"] = df["Word"].apply(lambda x: x.lower() if x.lower() in val else x)
     # TODO mask the most common words
     return df
 
@@ -41,4 +45,6 @@ def replicas_interactions(base: pd.DataFrame):
 
 if __name__ == "__main__":
     b = replicas_base()
-    replicas_words(b)
+    w = replicas_words(b)
+    g = w.groupby("Word").count()
+    pass
